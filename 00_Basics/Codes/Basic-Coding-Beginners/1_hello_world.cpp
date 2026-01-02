887c9ac15ee62b4c0553055baf02fbdb82fbb490
Tokachichu Sri Charan <tokachichusricharan2005@gmail.com>
2026-01-02 19:20:30 +0530

    Create 1_hello_world.cpp

---
+++ 00_Basics/Codes/Basic-Coding-Beginners/1_hello_world.cpp
@@ -0,0 +1,45 @@
+/*
+---------------------------------------------------------
+C++ Hello World
+---------------------------------------------------------
+
+This problem is solved in two formats:
+
+1) Platform-specific format (GeeksforGeeks)
+   - Uses class Solution and helloWorld() function
+   - This is the ACTIVE solution used for submission
+
+2) Normal C++ program format
+   - Uses main() function
+   - Kept for learning and reference purposes
+---------------------------------------------------------
+*/
+
+// =====================
+// GeeksforGeeks Solution
+// =====================
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    void helloWorld() {
+        cout << "Hello World";
+    }
+};
+
+/*
+====================================
+Normal C++ Program (For Understanding)
+====================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World";
+    return 0;
+}
+
+*/
